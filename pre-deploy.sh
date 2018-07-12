faf1bc0761ee905c4734008b2d37f6c60e32a182
Tim <dev@hempcoin.org>
2018-07-13 01:57:01 +0200

    osx fix

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -6,27 +6,31 @@ mkdir -p $OUTDIR/bin
 ARCHIVE_CMD="zip"
 
 if [[ $HOST = "i686-w64-mingw32" ]]; then
-  ARCHIVE_NAME="windows-x86.zip"
+  ARCHIVE_NAME="thc-windows-x86.zip"
 elif [[ $HOST = "x86_64-w64-mingw32" ]]; then
-    ARCHIVE_NAME="windows-x64.zip"
+    ARCHIVE_NAME="thc-windows-x64.zip"
 elif [[ $HOST = "arm-linux-gnueabihf" ]]; then
-    ARCHIVE_NAME="arm-x86.tar.gz"
+    ARCHIVE_NAME="thc-arm-x86.tar.gz"
     ARCHIVE_CMD="tar -czf"
 elif [[ $HOST = "aarch64-linux-gnu" ]]; then
-    ARCHIVE_NAME="arm-x64.tar.gz"
+    ARCHIVE_NAME="thc-arm-x64.tar.gz"
     ARCHIVE_CMD="tar -czf"
-elif [[ $HOST = "x86_64-unknown-linux-gnu" ]]; then
+elif [[ $HOST = "thc-x86_64-unknown-linux-gnu" ]]; then
     ARCHIVE_NAME="linux-x64.tar.gz"
     ARCHIVE_CMD="tar -czf"
-elif [[ $HOST = "x86_64-apple-darwin11" ]]; then
+elif [[ $HOST = "thc-x86_64-apple-darwin11" ]]; then
     ARCHIVE_NAME="osx-x64.zip"
 fi
 
-cp $TRAVIS_BUILD_DIR/src/qt/hempcoin-qt $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/qt/hempcoin-qt.exe $OUTDIR/bin/ || echo "no QT Wallet"
-cp $TRAVIS_BUILD_DIR/src/hempcoind $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/hempcoind.exe $OUTDIR/bin/
-cp $TRAVIS_BUILD_DIR/src/hempcoin-cli $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/hempcoin-cli.exe $OUTDIR/bin/
-strip "$OUTDIR/bin"/* || echo "nothing to strip"
-ls -lah $OUTDIR/bin
+if [[ $HOST = "x86_64-apple-darwin11" ]]; then
+    find $TRAVIS_BUILD_DIR -type f | grep -i hempcoin-Core.dmg$ | xargs -i cp {} $OUTDIR/bin
+else
+    cp $TRAVIS_BUILD_DIR/src/qt/hempcoin-qt $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/qt/hempcoin-qt.exe $OUTDIR/bin/ || echo "no QT Wallet"
+    cp $TRAVIS_BUILD_DIR/src/hempcoind $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/hempcoind.exe $OUTDIR/bin/
+    cp $TRAVIS_BUILD_DIR/src/hempcoin-cli $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/hempcoin-cli.exe $OUTDIR/bin/
+    strip "$OUTDIR/bin"/* || echo "nothing to strip"
+    ls -lah $OUTDIR/bin
+fi
 
 cd $OUTDIR/bin
 ARCHIVE_CMD="$ARCHIVE_CMD $ARCHIVE_NAME *"
