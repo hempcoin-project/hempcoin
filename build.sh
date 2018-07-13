dff2214e59db2b0d2b581bff38b14f47b92e04b9
Tim <dev@hempcoin.org>
2018-07-13 02:37:24 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 ./autogen.sh
 cd depends && make $MAKEJOBS $OPT
 cd $TRAVIS_BUILD_DIR && ./configure --prefix=$TRAVIS_BUILD_DIR/depends/$HOST $CONF
-make $MAKEJOBS
+make $MAKEJOBS $GOAL
 echo "all done"
 exit 0
