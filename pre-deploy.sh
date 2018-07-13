9657026645bf1c01f4559da8c12b3165b6eaf242
Tim <dev@hempcoin.org>
2018-07-13 03:14:27 +0200

    Update pre-deploy.sh

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -3,6 +3,8 @@
 OUTDIR=$TRAVIS_BUILD_DIR/out/$TRAVIS_PULL_REQUEST/$TRAVIS_JOB_NUMBER-$HOST
 mkdir -p $OUTDIR/bin
 
+echo $TRAVIS_BUILD_DIR
+
 ARCHIVE_CMD="zip"
 
 if [[ $HOST = "i686-w64-mingw32" ]]; then
@@ -15,15 +17,15 @@ elif [[ $HOST = "arm-linux-gnueabihf" ]]; then
 elif [[ $HOST = "aarch64-linux-gnu" ]]; then
     ARCHIVE_NAME="thc-arm-x64.tar.gz"
     ARCHIVE_CMD="tar -czf"
-elif [[ $HOST = "thc-x86_64-unknown-linux-gnu" ]]; then
-    ARCHIVE_NAME="linux-x64.tar.gz"
+elif [[ $HOST = "x86_64-unknown-linux-gnu" ]]; then
+    ARCHIVE_NAME="thc-linux-x64.tar.gz"
     ARCHIVE_CMD="tar -czf"
-elif [[ $HOST = "thc-x86_64-apple-darwin11" ]]; then
-    ARCHIVE_NAME="osx-x64.zip"
+elif [[ $HOST = "x86_64-apple-darwin11" ]]; then
+    ARCHIVE_NAME="thc-osx-x64.zip"
 fi
 
 if [[ $HOST = "x86_64-apple-darwin11" ]]; then
-    find $TRAVIS_BUILD_DIR -type f | grep -ir *-*.dmg$ | xargs -i cp {} $OUTDIR/bin
+    find /home/travis/build/hempcoin-project/hempcoin/ -type f | grep -r -i *.dmg$ | xargs -i cp {} $OUTDIR/bin
 else
     cp $TRAVIS_BUILD_DIR/src/qt/hempcoin-qt $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/qt/hempcoin-qt.exe $OUTDIR/bin/ || echo "no QT Wallet"
     cp $TRAVIS_BUILD_DIR/src/hempcoind $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/hempcoind.exe $OUTDIR/bin/
